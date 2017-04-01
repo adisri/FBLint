43c2d6486b5478ffe6f4a3c0c11e31fe4317affe
Aditya Srinivasan <asrinivasan@ucla.edu>
2017-04-01 16:11:48 -0700

    Code cleanup and got alert sending when period key pressed.

--- content.js
+++ content.js
@@ -1,57 +1,23 @@
 function setUpEventListener(){
-	var $node = $('div._5rpb');
-	console.log($node);
-	if($node) {
-		var $newNode = $node.clone(true);
-		console.log($newNode);
-		var $placeholderNode = $('[id^="placeholder"]');
-		// var $placeholderNode = getElementsStartsWithId('placeholder');
-		if($placeholderNode)
-			$placeholderNode.remove();
-
-
-		$newNode.keyup(function(event){
-			// api call here 190
-			switch(event.keyCode) {
-				case 190:
-					console.log("period");
-					break;
-				case 13:
-					console.log("enter"); // TODO: suppress newline
-					sendAlert();
-					break;
+	$(document).keyup(function(event){
+		var $node = $("span[data-text='true']");
+		if ($node) {
+			if (event.keyCode === 190) { // period key
+				sendAlert();
 			}
-			console.log("here");
-		});
-
-		console.log($newNode);
-
-		$node.replaceWith($newNode);
-	}
+		}
+	});
 }
 
 function sendAlert() {
 	swal({
-	  title: 'Are you sure?',
-	  text: "This message could be hurtful. Are you sure you want to send it?",
+	  title: 'Wait!',
+	  text: "This message could be hurtful. Think twice before you send it!",
 	  type: 'warning',
-	  showCancelButton: true,
 	  confirmButtonColor: '#3085d6',
-	  cancelButtonColor: '#d33',
-	  confirmButtonText: "Yes, I'm sure.",
-	  cancelButtonText: 'No, cancel!',
-	  confirmButtonClass: 'btn btn-success',
-	  cancelButtonClass: 'btn btn-danger',
+	  confirmButtonText: "I understand the consequences.",
+	  confirmButtonClass: 'btn btn-danger',
 	  buttonsStyling: true
-	}).then(function () {
-		// TODO: send message
-		swal(
-	    'Sent!',
-	    'Your message has been sent.',
-	    'success'
-	  )
-	}, function (dismiss) {
-		// Nothing to do if cancel was hit
 	})
 }
 

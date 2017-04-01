badd827efb1ea7ff460dfb4bfa3898050f651285
haochiencho@gmail.com <haochiencho@gmail.com>
2017-04-01 02:58:15 -0700

    add event listeners

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-alert("Hello from your Chrome extension!");
+
+
+function myAddEventListener(){
+	var nodes = document.querySelector('div._kmc');
+	nodes.addEventListener("keyup", function(){
+		// api here
+		console.log("here");
+	});
+}
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    myAddEventListener();
+});
+

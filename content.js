01d91e0058e49052abd056b78a3396869641bb4b
haochiencho@gmail.com <haochiencho@gmail.com>
2017-04-01 10:01:20 -0700

    event listeners for period and enter

--- content.js
+++ content.js
@@ -1,15 +1,33 @@
 
 
-function myAddEventListener(){
-	var nodes = document.querySelector('div._kmc');
-	nodes.addEventListener("keyup", function(){
-		// api here
-		console.log("here");
-	});
+function setUpEventListener(){
+	var node = document.querySelector('div._5rpb');
+	console.log(node);
+	if(node){
+		// var newNode = node.cloneNode(true);
+		// node.parentNode.replaceChild(newNode, node);
+		// console.log(newNode);
+		node.addEventListener("keyup", function(event){
+			// api call here 190
+			switch(event.keyCode) {
+				case 190:
+					console.log("period");
+					break;
+				case 13:
+					console.log("enter");
+
+					// event.preventDefault();
+					break;
+			}
+			console.log("here");
+		});
+
+	}
+
 }
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    myAddEventListener();
+    setUpEventListener();
 });
 

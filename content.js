e6abba9bc6cea9fe3350fe7f9d67d381bead949b
haochiencho@gmail.com <haochiencho@gmail.com>
2017-04-01 10:58:15 -0700

    finer granularity

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 
 
 function setUpEventListener(){
-	var $node = $("div._5rpb");
+	var $node = $('div._kmc');
 	console.log($node);
 	if($node) {
-		// var newNode = node.cloneNode(true);
-		// node.parentNode.replaceChild(newNode, node);
-		// console.log(newNode);
-		$node.keyup(function(event){
+		var $newNode = $node.clone(true);
+		console.log($newNode);
+
+		$newNode.keyup(function(event){
 			// api call here 190
 			switch(event.keyCode) {
 				case 190:
@@ -20,6 +20,10 @@ function setUpEventListener(){
 			}
 			console.log("here");
 		});
+
+		console.log($newNode);
+
+		$node.replaceWith($newNode);
 	}
 }
 
@@ -27,3 +31,39 @@ $(document).ready(function() {
     console.log("ready!");
     setUpEventListener();
 });
+
+
+
+
+// function setUpEventListener(){
+// 	var node = document.querySelector('div._1mf');
+// 	console.log(node);
+// 	console.log()
+// 	if(node){
+// 		// var newNode = node.cloneNode(true);
+// 		// node.parentNode.replaceChild(newNode, node);
+// 		// console.log(newNode);
+// 		node.addEventListener("keyup", function(event){
+// 			// api call here 190
+// 			switch(event.keyCode) {
+// 				case 190:
+// 					console.log("period");
+// 					break;
+// 				case 13:
+// 					console.log("enter");
+
+// 					// event.preventDefault();
+// 					break;
+// 			}
+// 			console.log("here");
+// 		});
+
+// 	}
+
+// }
+
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+//     setUpEventListener();
+// });
+

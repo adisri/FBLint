dbbfd903887d525898599903710929b195b3bbce
Aditya Srinivasan <asrinivasan@ucla.edu>
2017-04-01 19:48:19 -0700

    Fixed API call issue with dynamic string usage

--- content.js
+++ content.js
@@ -4,14 +4,11 @@ function setUpEventListener(){
 	$(document).keyup(function(event){
 		var $node = $("span[data-text='true']");
 		if ($node) {
+			console.log($node.text());
 			if (event.keyCode === 190 || (event.keyCode === 191 && event.shiftKey) || (event.keyCode === 49 && event.shiftKey)) { // period, question mark, exclamation
 				var sendRequestTextAnalytics =
 					sendRequestGenerator('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', textAnalyticsSuccessFunction, config.TEXT_API_KEY);
-				//sendRequestTextAnalytics("{'documents': [{'language': 'en','id': '1','text':" + $node.text() + "}]}");
-				var sendRequestLinguistics =
-					sendRequestGenerator('https://westus.api.cognitive.microsoft.com/linguistics/v1.0/analyze', linguisticsSuccessFunction, config.LINGUISTICS_API_KEY);
-				sendRequestLinguistics("{'documents': [{'language': 'en','id': '2','text': 'You are a terrible human being.' }]}");
-
+				sendRequestTextAnalytics("{'documents': [{'language': 'en','id': '2','text':'" + $node.text() + "'}]}");
 				sendAlert();
 			}
 		}
@@ -22,10 +19,6 @@ function textAnalyticsSuccessFunction(data, textStatus, jqXHR){
 	console.log(data);
 	console.log(data.documents[0].score);
 	textAnalyticsScore = data.documents[0].score;
-}
-
-function linguisticsSuccessFunction(data, textStatus, jqXHR){
-	console.log('hit here');
 }
 
 function sendRequestGenerator(API_URL, successFunction, API_KEY){
@@ -44,7 +37,7 @@ function sendRequestGenerator(API_URL, successFunction, API_KEY){
 function sendAlert() {
 	swal({
 	  title: 'Wait!',
-	  text: "This message could be hurtful. Think twice before you send it!",
+	  text: "This message could be perceived as harmful. Think twice before you send it!",
 	  type: 'warning',
 	  confirmButtonColor: '#3085d6',
 	  confirmButtonText: "I understand the consequences.",
